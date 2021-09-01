346f0bbee31cd28993b4d52576913eff84978e76
Mauro Condarelli <mc5686@mclink.it>
2021-09-01 15:10:54 +0200

    First attempt to anall-in-one container.

---
+++ test-it.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+dest=$(pwd)/permanent
+[ -d "$dest" ] || mkdir "$dest"
+
+[ -e Dockerfile ] || ln -s docker/all-in-one/Dockerfile.debian Dockerfile
+docker build -t mcondarelli/zt1-ui:0.1 -t mcondarelli/zt1-ui:latest  .
+docker run -d --rm --name zt1 \
+  --mount "type=bind,src=$dest,dst=/var/lib/zerotier-one" \
+  -p 4000:4000 \
+  -e ZU_DEFAULT_USERNAME=admin \
+  -e ZU_DEFAULT_PASSWORD=zero-ui \
+  -e ZU_CONTROLLER_ENDPOINT=http://localhost:9993/\
+  mcondarelli/zt1-ui
+authtoken=$(docker exec -ti zt1 cat /var/lib/zerotier-one/authtoken.secret)
+
+#docker exec -it zt1 curl -H "X-ZT1-Auth:$authtoken" http://127.0.0.1:9993/status
+#docker exec -it zt1 curl -H "X-ZT1-Auth:$authtoken" http://127.0.0.1:9993/controller
+
+#docker stop zt1
