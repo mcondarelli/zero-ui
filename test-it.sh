09b83681520ef05401b798cd757599ad6f5403d8
Mauro Condarelli <mc5686@mclink.it>
2021-09-06 23:13:13 +0200

    Network data VOLUME handling

--- test-it.sh
+++ test-it.sh
@@ -4,11 +4,16 @@ set -e
 
 dest=$(pwd)/permanent
 [ -d "$dest" ] || mkdir "$dest"
+data=$dest/data
+[ -d "$data" ] || mkdir "$data"
 
 [ -e Dockerfile ] || ln -s docker/all-in-one/Dockerfile.debian Dockerfile
 docker build -t mcondarelli/zt1-ui:0.1 -t mcondarelli/zt1-ui:latest  .
 docker run -d --rm --name zt1 \
   --mount "type=bind,src=$dest,dst=/var/lib/zerotier-one" \
+  --mount "type=bind,src=$data,dst=/app/backend/data" \
+  --net=host --device=/dev/net/tun \
+  --cap-add=NET_ADMIN --cap-add=SYS_ADMIN \
   -p 4000:4000 \
   -e ZU_DEFAULT_USERNAME=admin \
   -e ZU_DEFAULT_PASSWORD=zero-ui \
